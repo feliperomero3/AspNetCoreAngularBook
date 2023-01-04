c9a72f4fb7bcbb6ad6e45836d8883bc3e08fc5a1
Felipe Romero <feliperomero3@outlook.com>
2023-01-03 22:07:07 -0600

    Refactoring the Angular app — Adding FetchDataComponent.

--- src/HealthCheck/src/app/app.module.ts
+++ src/HealthCheck/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { FetchDataComponent } from './fetch-data/fetch-data.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    FetchDataComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
