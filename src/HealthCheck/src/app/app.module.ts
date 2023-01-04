743374931c46278ca01d1c20f7fbb6596897e431
Felipe Romero <feliperomero3@outlook.com>
2023-01-03 22:15:04 -0600

    Refactoring the Angular app — Adding the navigation menu.

--- src/HealthCheck/src/app/app.module.ts
+++ src/HealthCheck/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { FetchDataComponent } from './fetch-data/fetch-data.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    FetchDataComponent
+    FetchDataComponent,
+    NavMenuComponent
   ],
   imports: [
     BrowserModule, HttpClientModule
