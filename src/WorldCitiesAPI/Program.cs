f4f908e39d8945ef8ef34a6510a94a662028e8ce
Felipe Romero <feliperomero3@outlook.com>
2023-01-10 22:15:50 -0600

    Add EF Core in the pipeline.

--- src/WorldCitiesAPI/Program.cs
+++ src/WorldCitiesAPI/Program.cs
@@ -1,8 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+using WorldCitiesAPI.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
