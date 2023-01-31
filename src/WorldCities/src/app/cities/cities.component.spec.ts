93dca2d027ef732f5582e35502f1f9ac2f36870b
Felipe Romero <feliperomero3@outlook.com>
2023-01-30 22:25:45 -0600

    Add cities component's unit tests.

--- src/WorldCities/src/app/cities/cities.component.spec.ts
+++ src/WorldCities/src/app/cities/cities.component.spec.ts
@@ -1,15 +1,25 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CitiesComponent } from './cities.component';
+import { City } from './city';
 
 describe('CitiesComponent', () => {
   let component: CitiesComponent;
   let fixture: ComponentFixture<CitiesComponent>;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [CitiesComponent]
+      imports: [
+        HttpClientTestingModule
+      ],
+      declarations: [
+        CitiesComponent
+      ]
     }).compileComponents();
+
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   beforeEach(() => {
@@ -18,7 +28,15 @@ describe('CitiesComponent', () => {
     fixture.detectChanges();
   });
 
-  it('should create the cities', () => {
+  it('should create the cities component', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should get the cities', () => {
+    const testData: City[] = [{ cityId: 1, name: 'Tokyo', latitude: 100, longitude: 200 }];
+    const req = httpTestingController.expectOne('api/cities');
+    req.flush(testData);
+    httpTestingController.verify();
+    expect(component.cities.length).toBe(1);
   });
 });
