2bec2a2316387cf5c8686c7014aeae057a42f542
Felipe Romero <feliperomero3@outlook.com>
2023-02-13 22:57:39 -0600

    Class inheritance — Implementing a BaseFormComponent.

---
+++ src/WorldCities/src/app/base-form.component.ts
@@ -0,0 +1,30 @@
+import { Component } from '@angular/core';
+import { FormGroup, AbstractControl } from '@angular/forms';
+
+@Component({
+  template: ''
+})
+export abstract class BaseFormComponent {
+  form!: FormGroup;
+
+  getErrors(control: AbstractControl, displayName: string): string[] {
+    var errors: string[] = [];
+    Object.keys(control.errors || {}).forEach((key) => {
+      switch (key) {
+        case 'required':
+          errors.push(`${displayName} is required.`);
+          break;
+        case 'pattern':
+          errors.push(`${displayName} contains invalid characters and/or length.`);
+          break;
+        case 'isDuplicatedField':
+          errors.push(`${displayName} already exists: please choose another.`);
+          break;
+        default:
+          errors.push(`${displayName} is invalid.`);
+          break;
+      }
+    });
+    return errors;
+  }
+}
