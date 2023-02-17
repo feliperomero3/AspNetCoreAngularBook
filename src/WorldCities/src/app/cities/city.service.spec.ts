b6e342d787f5744e2188cac5017285127ecf06bc
Felipe Romero <feliperomero3@outlook.com>
2023-02-16 19:18:32 -0600

    Building a data service — Creating CityService.

---
+++ src/WorldCities/src/app/cities/city.service.spec.ts
@@ -0,0 +1,21 @@
+import { TestBed } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+
+import { CityService } from './city.service';
+
+describe('CityService', () => {
+  let service: CityService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ]
+    });
+    service = TestBed.inject(CityService);
+  });
+
+  it('should be created', () => {
+    expect(service).toBeTruthy();
+  });
+});
