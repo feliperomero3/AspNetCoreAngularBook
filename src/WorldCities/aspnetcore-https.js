8cd61fb256fbf9bae096b53b98f27c4ca3cf6290
Felipe Romero <feliperomero3@outlook.com>
2023-02-16 21:12:01 -0600

    Update aspnetcore-https.js

--- src/WorldCities/aspnetcore-https.js
+++ src/WorldCities/aspnetcore-https.js
@@ -12,7 +12,7 @@ const certificateArg = process.argv.map(arg => arg.match(/--name=(?<value>.+)/i)
 const certificateName = certificateArg ? certificateArg.groups.value : process.env.npm_package_name;
 
 if (!certificateName) {
-  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.')
+  console.error('Invalid certificate name. Run this script in the context of an npm/yarn script or pass --name=<<app>> explicitly.');
   process.exit(-1);
 }
 
@@ -29,5 +29,5 @@ if (!fs.existsSync(certFilePath) || !fs.existsSync(keyFilePath)) {
     'Pem',
     '--no-password',
   ], { stdio: 'inherit', })
-  .on('exit', (code) => process.exit(code));
+    .on('exit', (code) => process.exit(code));
 }
