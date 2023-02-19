f36d6bfc16519b3ab76a50b43c22533095aaef93
Felipe Romero <feliperomero3@outlook.com>
2023-02-18 23:19:24 -0600

    Update CityService tests.

--- src/WorldCities/src/app/cities/city.service.spec.ts
+++ src/WorldCities/src/app/cities/city.service.spec.ts
@@ -1,10 +1,11 @@
 import { TestBed } from '@angular/core/testing';
-import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 import { CityService } from './city.service';
 
 describe('CityService', () => {
   let service: CityService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
@@ -13,9 +14,16 @@ describe('CityService', () => {
       ]
     });
     service = TestBed.inject(CityService);
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should get the cities', () => {
+    service.getData(0, 10, 'name', 'asc', null, null).subscribe();
+    httpTestingController.expectOne('api/cities?pageIndex=0&pageSize=10&sortColumn=name&sortOrder=asc');
+    httpTestingController.verify();
   });
 });
