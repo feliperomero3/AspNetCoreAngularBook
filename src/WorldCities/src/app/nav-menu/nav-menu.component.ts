e8e38f28f83947a6f2b76200500c1cd309d70d7c
Felipe Romero <feliperomero3@outlook.com>
2023-02-24 21:23:13 -0600

    Adding the authentication status observable — Updating the UI.

--- src/WorldCities/src/app/nav-menu/nav-menu.component.ts
+++ src/WorldCities/src/app/nav-menu/nav-menu.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Subject, takeUntil } from 'rxjs';
+import { AuthenticationService } from '../authentication/authentication.service';
 
 @Component({
   selector: 'app-nav-menu',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-menu.component.scss']
 })
 export class NavMenuComponent implements OnInit {
+  private destroySubject = new Subject();
+  isLoggedIn = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(private router: Router, private authentication: AuthenticationService) {
+    this.isLoggedIn = this.authentication.isAuthenticated;
   }
 
+  ngOnInit(): void {
+    this.updateLoginStatus();
+  }
+
+  updateLoginStatus(): void {
+    this.authentication.authenticationStatus
+      .pipe(takeUntil(this.destroySubject))
+      .subscribe(result => {
+        this.isLoggedIn = result;
+      })
+  }
+
+  onLogout(): void {
+    this.authentication.logout();
+    this.router.navigate(["/"]);
+  }
 }
