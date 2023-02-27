64dc63ca9ef28e463bd0dde60da448e3cff8a907
Felipe Romero <feliperomero3@outlook.com>
2023-02-26 22:56:41 -0600

    Update logging configuration.

--- src/WorldCitiesAPI/Configurations/SerilogConfiguration.cs
+++ src/WorldCitiesAPI/Configurations/SerilogConfiguration.cs
@@ -12,7 +12,6 @@ public static class SerilogConfiguration
             .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", Serilog.Events.LogEventLevel.Debug)
             .MinimumLevel.Override("Microsoft.AspNetCore.Authorization", Serilog.Events.LogEventLevel.Debug)
             .MinimumLevel.Override("Microsoft.AspNetCore.Identity", Serilog.Events.LogEventLevel.Debug)
-            .MinimumLevel.Override("Microsoft.EntityFrameworkCore", Serilog.Events.LogEventLevel.Information)
             .WriteTo.Debug(outputTemplate: "{SourceContext} [{Level}] {Message:lj}{NewLine}{Exception}")
             .WriteTo.Console(outputTemplate: "{SourceContext} [{Level}] {Message:lj}{NewLine}{Exception}");
 }
